f0fd37faf15f38b91afc8b361523a24ad57ddb0a
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2023-06-15 19:22:11 -0300

    fix: formatando com prettier

--- src/mongoose/models/user.js
+++ src/mongoose/models/user.js
@@ -1,9 +1,9 @@
 import mongoose, { Schema } from 'mongoose';
 
 const UserSchema = new Schema({
-    'name': String,
-    'password': String,
-    'created_at': Date
+  name: String,
+  password: String,
+  created_at: Date,
 });
 
 export default mongoose.model('User', UserSchema);
