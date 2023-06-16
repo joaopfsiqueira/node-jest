dd01bf959892f04e16591c17b510ce0b1398ea64
João Pedro Siqueira <joaopfsiqueiraa@gmail.com>
2023-06-16 20:31:56 -0300

    feat: criando controller de user

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -1,8 +1,11 @@
-import user from '../mongoose/models/user';
+import users from '../mongoose/models/user';
 
 exports.insertUsers = async (req, res) => {
   const params = req.body;
 
+  const user = new users(params);
+  await user.save();
+  res.status(201).json('user created successfully');
   try {
   } catch (error) {
     return res.status(400).json(error.message);
@@ -10,9 +13,9 @@ exports.insertUsers = async (req, res) => {
 };
 
 exports.getUsers = async (req, res) => {
-  const params = req.body;
-
   try {
+    const allUsers = await users.find({});
+    res.status(200).json(allUsers);
   } catch (error) {
     return res.status(400).json(error.message);
   }
