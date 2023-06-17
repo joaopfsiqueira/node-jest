12c3628294cbbcba9f4f895f500ad5b603a41204
João Pedro Siqueira <joaopfsiqueiraa@gmail.com>
2023-06-17 06:57:45 -0300

    fix: ajustando controllers

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -1,22 +1,26 @@
-import users from '../mongoose/models/user';
+import users from '../mongoose/models/user.js';
 
-exports.insertUsers = async (req, res) => {
-  const params = req.body;
+const usersController = {
+  createUsers: async (req, res) => {
+    const params = req.body;
 
-  const user = new users(params);
-  await user.save();
-  res.status(201).json('user created successfully');
-  try {
-  } catch (error) {
-    return res.status(400).json(error.message);
-  }
+    const user = new users(params);
+    await user.save();
+    res.status(201).json('user created successfully');
+    try {
+    } catch (error) {
+      return res.status(400).json(error.message);
+    }
+  },
+
+  getUsers: async (req, res) => {
+    try {
+      const allUsers = await users.find({});
+      res.status(200).json(allUsers);
+    } catch (error) {
+      return res.status(400).json(error.message);
+    }
+  },
 };
 
-exports.getUsers = async (req, res) => {
-  try {
-    const allUsers = await users.find({});
-    res.status(200).json(allUsers);
-  } catch (error) {
-    return res.status(400).json(error.message);
-  }
-};
+export default usersController;
