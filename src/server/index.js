25eb195a51edf58a20a5c313cca0527130c90bec
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2023-06-17 07:14:18 -0300

    fix: ajustando import do dotenv

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import * as dotenv from 'dotenv';
+import dotenv from 'dotenv';
 import connection from '../mongoose/connection.js';
 import userRouter from '../routes/user.routes.js';
 
