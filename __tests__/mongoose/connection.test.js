894f07de917699d65028e0fa3a7fe5f721a88f19
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2023-06-19 16:06:44 -0300

    feat: criando connection.test do mongoose

---
+++ __tests__/mongoose/connection.test.js
@@ -0,0 +1,28 @@
+import { MongoClient } from 'mongodb';
+
+describe('insert', () => {
+  let connection;
+  let db;
+
+  beforeAll(async () => {
+    connection = await MongoClient.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    db = await connection.db('test-collection');
+  });
+
+  afterAll(async () => {
+    await connection.close();
+  });
+
+  it('should insert a doc into collection', async () => {
+    const users = db.collection('users');
+
+    const mockUser = { _id: 'some-user-id', name: 'John' };
+    await users.insertOne(mockUser);
+
+    const insertedUser = await users.findOne({ _id: 'some-user-id' });
+    expect(insertedUser).toEqual(mockUser);
+  });
+});
