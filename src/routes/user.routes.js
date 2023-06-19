fa0e1101a4136dc0507ac7b37591f9c6912424ad
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2023-06-19 16:06:34 -0300

    fix: ajustando arquivo

--- src/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import usersController from '../controller/user-controller.js';
-
-const userRouter = Router();
-
-userRouter.get('/', usersController.getUsers);
-userRouter.post('/', usersController.createUsers);
-
-export default userRouter;
